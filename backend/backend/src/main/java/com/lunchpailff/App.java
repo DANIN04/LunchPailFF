ba9dc6e0296df9c3e0f62f057fef4ac55224fa36
DANIN04 <indaniel04@outlook.com>
2024-12-19 10:29:17 -0800

    ablle to connect to localhost:7000

--- backend/backend/src/main/java/com/lunchpailff/App.java
+++ backend/backend/src/main/java/com/lunchpailff/App.java
@@ -1,13 +1,11 @@
 package com.lunchpailff;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import io.javalin.Javalin;
+
+public class App {
+
+    public static void main(String[] args) {
+        Javalin app = Javalin.create().start(7000);
+        app.get("/", ctx -> ctx.result("Welcome to LunchPailFF Backend!"));
     }
 }
